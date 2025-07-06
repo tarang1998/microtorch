af2526e0c6f239be595594e125e6bc2ddaf58027
Tarang <tarangnair98@gmail.com>
2025-07-06 16:27:35 -0400

    Setting up framework

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="microtorch",
+    version="0.1.0",
+    author="Tarang Nair",
+    description="Artificial Intelligence library",
+    packages=find_packages(),
+    install_requires=[],  # Add any required packages
+    python_requires=">=3.7",
+)
